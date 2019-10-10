8f0dbef88db9400a08cb83fc52abadabfc69d1de
Denis <presetslr@gmail.com>
2019-10-11 01:20:36 +0300

    init: start solution

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,50 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) throw Error("throws an exception if config isn\'t passed");
+        this.config = config;
+        this.state = config.initial;
+        this.history = [];
+
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (this.config.states[state]) {
+            this.state = state;
+            this.history.push(this.state);
+        } else
+            throw Error('changeState exception');
+    }
+
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        this.changeState(this.config.states[this.state].transitions[event]);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +54,45 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let State = [];
+        if (event) {
+            for (let state in this.config.states) {
+                if(this.config.states[state].transitions[event])
+                    State.push(state);
+            }
+        } else {
+            for (let state in this.config.states) {
+                State.push(state);
+            }
+        }
+        return State;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        return this.state === this.config.initial ? false : true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        return this.state === this.config.initial ? false : true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history.splice();
+    }
 }
 
 module.exports = FSM;
